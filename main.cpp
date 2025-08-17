cefbcc0703ffb82f108b0a5cf2db931798423c2a
Juan Rojas <sebasr42@javerianacali.edu.co>
2025-08-17 14:54:32 -0500

    Implementacion de Entrada dinámica con std::cin para Variables X y Y

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 using namespace std;
 
 int main() {
-    int x = 10;
-    int y = 4;
+    int x, y;
+
+    // Ingreso dinámico de los valores
+    cout << "Ingrese el primer numero: ";
+    cin >> x;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> y;
 
     int resultado_suma = sumar(x, y);
     int resultado_resta = restar(x, y);
     int resultado_multiplicacion = multiplicar(x, y);
     int resultado_dividir = dividir(x, y);
 
-
     cout << "Suma: " << resultado_suma << endl;
     cout << "Resta: " << resultado_resta << endl;
     cout << "Multiplicacion: " << resultado_multiplicacion << endl;
     cout << "Division: " << resultado_dividir << endl;
-
 
     return 0;
 }
